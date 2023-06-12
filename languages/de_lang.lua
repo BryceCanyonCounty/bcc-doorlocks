5475b53cf644a42ebfce014ff44ab949b3a545cc
Charly <124067328+MehonTheMan@users.noreply.github.com>
2023-06-13 01:16:44 +0200

    de_lang
    German language

---
+++ languages/de_lang.lua
@@ -0,0 +1,30 @@
+---------------------------------- GERMAN -------------------------------------------
+Locales["de_lang"] = {
+    --Platzhalter
+    failedToCreateDoor = 'Keine Tür gefunden',
+    doorCreated = 'Tür erfolgreich erstellt!',
+    doorExists = 'Die Tür ist bereits verschlossen!',
+    createDoorInstructions = 'Ziele auf die Tür und drücke "G" während du auf Sie zielst, um Sie zu erstellen!',
+    deleteDoorInstructions = 'Ziele auf die Tür und drücke "G" während du auf Sie zielst, um Sie zu löschen!',
+    questionLocking = 'Die Tür verschließen?',
+    questionDeletion = 'Die Tür löschen?',
+    doorDeleted = 'Tür gelöscht!',
+
+    ----Non admin text ----
+    doorManage = 'Tür',
+    unlockDoor = 'Tür aufschließen',
+    lockpickDoor = 'Tür aufbrechen',
+    noLockpick = 'Du hast keinen Dietrich!',
+    lockPicked = 'Du hast die Tür aufgebrochen!',
+    lockDoor = 'Tür schließen',
+
+    ---- Door Creation Menu ---
+    menuTitle = 'Tür erstellen',
+    setJob = 'Job setzen',
+    setJob_desc = 'Setze einen Job, der die Tür auf und zuschließen kann!',
+    setKeyItem = 'Schlüsselitem',
+    setKeyItem_desc = 'Setze ein Item, damit du die Tür auf und zuschließen kannst. Es muss dem Namen in der DB entsprechen!',
+    confirm = 'Bestätigen',
+    confirm_desc = 'Türerstellung beenden!',
+    InvalidInput = 'falsche Eingabe'
+}
