90b9b52665c5dc23c3ed2a26a256db3c37d126c4
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-12-09 21:42:58 -0800

    Update config.lua
    - add door radius config setting

--- config.lua
+++ config.lua
@@ -6,6 +6,8 @@ Config.DevMode = false --false on live server
 Config.CreateDoorCommand = 'createDoor' --command to create door
 Config.DeleteDoorCommand = 'deleteDoor' --command to delete door
 
+Config.DoorRadius = 1.5 -- Maximum Distance from Door to Operate
+
 ---------- Admin Configuration (Anyone listed here will be able to create and delete doors!) -----------
 Config.AdminSteamIds = {
     {
