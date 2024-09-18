1d5787126c7a24c726686ab4b867b22fb203ca1c
Zeus Script <51821404+Z-eus@users.noreply.github.com>
2024-09-18 03:19:30 +0300

    Update config.lua (#36)
    Fix config.lua

--- config.lua
+++ config.lua
@@ -1,17 +1,17 @@
 Config = {}
 
-Config.defaultlang = 'en_lang' --set your language here current supported languages: "en_lang" = english
-Config.DevMode = false --false on live server
+Config.defaultlang = 'en_lang' -- Set your language here current supported languages: "en_lang" = english
+Config.DevMode = false -- false on live server
 
-Config.CreateDoorCommand = 'createDoor' --command to create door
-Config.DeleteDoorCommand = 'deleteDoor' --command to delete door
+Config.CreateDoorCommand = 'createDoor' -- Command to create door
+Config.DeleteDoorCommand = 'deleteDoor' -- Command to delete door
 
 Config.DoorRadius = 1.5 -- Maximum Distance from Door to Operate
 
----------- Admin Configuration (Anyone listed here will be able to create and delete doors!) -----------
+---------- Admin Configuration (Anyone listed here will be able to create and delete doors!) ----------
 Config.AdminSteamIds = {
     {
-        steamid = 'steam:11000010401e04f', --insert players steam id
+        steamid = 'steam:12004500000a00b', -- Insert players steam id
     }, --to add more just copy this table paste and change id
 }
 
@@ -19,40 +19,11 @@ Config.LockPicking = {
     bcc_minigames = false,
     rsd_lockpick = true,
 
-    allowlockpicking = true, --If true players will be able to lockpick doors
+    allowlockpicking = true, -- If true players will be able to lockpick doors
     minigameSettings = {
-        MaxAttemptsPerLock = 3, -- only bcc_minigames
+        MaxAttemptsPerLock = 3, -- Only bcc_minigames
         lockpickitem = 'lockpick',
-        difficulty = 50, -- only bcc_minigames
-        hintdelay = 500, -- only bcc_minigames
-    },
-}
-Config = {}
-
-Config.defaultlang = 'en_lang'          --set your language here current supported languages: "en_lang" = english
-Config.DevMode = false                  --false on live server
-
-Config.CreateDoorCommand = 'createDoor' --command to create door
-Config.DeleteDoorCommand = 'deleteDoor' --command to delete door
-
-Config.DoorRadius = 1.5                 -- Maximum Distance from Door to Operate
-
----------- Admin Configuration (Anyone listed here will be able to create and delete doors!) -----------
-Config.AdminSteamIds = {
-    {
-        steamid = 'steam:11000010401e04f', --insert players steam id
-    },                                     --to add more just copy this table paste and change id
-}
-
-Config.LockPicking = {
-    bcc_minigames = false,
-    rsd_lockpick = true,
-
-    allowlockpicking = true,    --If true players will be able to lockpick doors
-    minigameSettings = {
-        MaxAttemptsPerLock = 3, -- only bcc_minigames
-        lockpickitem = 'lockpick',
-        difficulty = 50,        -- only bcc_minigames
-        hintdelay = 500,        -- only bcc_minigames
+        difficulty = 50, -- Only bcc_minigames
+        hintdelay = 500, -- Only bcc_minigames
     },
 }
