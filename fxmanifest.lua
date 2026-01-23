485e8b466b2c6da0498a771da816f25489a9c5de
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-01-22 21:53:51 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,40 +1,39 @@
-fx_version "cerulean"
-games {"rdr3"}
-rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-lua54 "yes"
-
+game 'rdr3'
+lua54 'yes'
 author 'BCC Team'
 
-description 'Create doors for exiting houses in RedM!'
-
 shared_scripts {
-  "config.lua",
-  "locale.lua",
-  "languages/*.lua"
+    'config.lua',
+    'locale.lua',
+    'languages/*.lua'
 }
 
 server_scripts {
-  '@oxmysql/lib/MySQL.lua',
-  '/server/dbUpdater.lua',
-  '/server/API.lua',
-  "/server/server.lua"
+    '@oxmysql/lib/MySQL.lua',
+    'server/server_init.lua',
+    'server/dbUpdater.lua',
+    'server/API.lua',
+    'server/server.lua'
 }
 
 client_scripts {
-  "/client/functions.lua",
-  "/client/client.lua",
-  "/client/doorhashes.lua",
-  '/client/MenuSetup.lua'
+    'client/client_init.lua',
+    'client/functions.lua',
+    'client/client.lua',
+    'client/doorhashes.lua',
+    'client/MenuSetup.lua'
 }
 
 dependency {
-  'vorp_core',
-  'vorp_inventory',
-  'vorp_character',
-  'feather-menu',
-  'bcc-utils',
-  'bcc-minigames'
+    'vorp_core',
+    'vorp_inventory',
+    'vorp_character',
+    'feather-menu',
+    'bcc-utils',
+    'bcc-minigames'
 }
 
-version '1.1.2'
+version '1.2.0'
