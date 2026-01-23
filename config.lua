ed6ac90fa7a30c460469d1b0e8e5c9294ff7f190
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-01-22 21:53:41 -0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,35 +1,55 @@
 Config = {}
 
-Config.defaultlang = 'en_lang' -- Set your language here current supported languages: "en_lang" = english
+Config.defaultlang = 'en_lang' -- Set your language here
+-----------------------------------------------------
 
-Config.DevMode = false -- false on live server
+-- Enable to see debug messages in the client and server consoles
+Config.devMode = {
+    active = true -- Default: false / DO NOT Enable on Live Server
+}
+-----------------------------------------------------
+
+Config.Keys = {
+    lock = 0x760A9C6F, -- [G] lock/unlock
+    lockpick = 0xCEFD9220 -- [E]
+}
+-----------------------------------------------------
 
 Config.CloseOnRestart = true -- Close all doors on server restart
+-----------------------------------------------------
 
-Config.ManageDoorLocks = 'ManageDoorLocks'
+Config.ManageDoorLocks = 'ManageDoorLocks' -- Command to open door management menu
+-----------------------------------------------------
 
-Config.doorlocksDevCommand = 'doorlocksDev'
+Config.DoorRadius = 1.5 -- Default: 1.5 / Maximum Distance from Door to Operate
+-----------------------------------------------------
 
-Config.DoorRadius = 1.5 -- Maximum Distance from Door to Operate
-
----------- Admin Configuration (Any group listed here will be able to create and delete doors!) ----------
-Config.adminGroup = 'admin'
-
--- These are jobs that will be able to create doors just like the admins
-Config.AllowedJobs = {
-    {
-        jobname = '' --the job name
-    },
+-- These are groups that will be able to create, manage and operate doors (set in 'group' in 'characters' table)
+Config.AllowedGroups = {
+    { groupName = 'admin' }, -- Example: { groupName = 'admin'
 }
+
+-- These are jobs that will be able to create, manage and operate doors *Optional* (set in 'job' in 'characters' table)
+Config.AllowedJobs = {
+    { jobName = '' }, -- Example: { jobName = 'police' }
+}
+-----------------------------------------------------
 
 Config.LockPicking = {
     minigameScript = 'bcc_minigames', -- bcc_minigames or rsd_lockpick
     allowlockpicking = true, -- If true players will be able to lockpick doors
     minigameSettings = {
-        MaxAttemptsPerLock = 3,
-        lockpickitem = 'lockpick',
-        difficulty = 50, -- Only bcc_minigames
-        hintdelay = 500, -- Only bcc_minigames
+        MaxAttemptsPerLock = 3, -- How many fail attempts are allowed before game over
+        lockpickitem = 'lockpick', -- Item Name in Database
+        -- Only bcc_minigames settings
+        difficulty = 20, -- +- threshold to the stage degree (bigger number means easier)
+        hintdelay = 100, -- Only bcc_minigames
+        randomDegrees = true, -- If true, pins will be random degrees; if false, use static degrees
+        staticDegrees = {     -- Static degrees to use when randomDegrees is false
+            90,               -- Stage 1 degree (0-360)
+            180,              -- Stage 2 degree (0-360)
+            270,              -- Stage 3 degree (0-360)
+        },
     },
 }
 
